dcdc83054ffe48f580659ee24907447ada537da3
thrazer675 <thrazer675@aol.com>
2021-02-28 11:42:49 -0500

    updated html template to allow featured info

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
+//const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
+ //fs.writeFile('./index.html', pageHTML, err => {
+ //  if (err) throw err;
+ 
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
@@ -115,7 +116,13 @@ const inquirer = require('inquirer');
   };
 
   promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-      console.log(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
     });
+  });

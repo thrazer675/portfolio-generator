e47f919cc37fe892c05943c84cd449460ca26b97
thrazer675 <thrazer675@aol.com>
2021-03-02 10:59:45 -0500

    fixed issue with style sheet not copying and cleaned up code with more concise syntax

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 //const pageHTML = generatePage(name, github);
@@ -116,13 +116,20 @@ const generatePage = require('./src/page-template');
   };
 
   promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-     fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
+    .then(promptProject)
+    .then(portfolioData => {
+      return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+      return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+      console.log(writeFileResponse);
+      return copyFile();
+    })
+    .then(copyFileResponse => {
+      console.log(copyFileResponse);
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
--- src/page-template.js
+++ src/page-template.js
@@ -5,11 +5,11 @@ const generateAbout = aboutText => {
   }
 
   return `
-  <section class="my-3" id="about">
-    <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-    <p>${aboutText}</p>
-  </section>
-  `;
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+    `;
 };
 
 const generateProjects = projectsArr => {
@@ -78,9 +78,7 @@ module.exports = templateData => {
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-            header.github
-          }">GitHub</a>
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
         </nav>
       </div>
     </header>
